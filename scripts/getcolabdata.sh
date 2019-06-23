3139d84f43a7233d37953b9454bf221da8463072
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2019-06-23 17:37:56 +0100

    Colab script for getting data to colab enviroment

---
+++ scripts/getcolabdata.sh
@@ -0,0 +1,15 @@
+# This is a shell script for automaticly loading data inside colab enviroment.
+# Before uploading this script its vital to add kaggle.json file to enviroment with fallowing code.
+
+# from google.colab import files
+# files.upload()
+
+pip install -q kaggle
+mkdir -p ~/.kaggle
+cp kaggle.json ~/.kaggle/
+kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
+unzip chest-xray-pneumonia.zip
+unzip chest_xray.zip
+rm chest-xray-pneumonia.zip
+rm chest_xray.zip
+#rm -rf __MACOSX/
