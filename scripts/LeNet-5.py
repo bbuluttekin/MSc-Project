56c3f577e4d5be93a93fe7fdac9dd96e40cbf253
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2020-02-29 22:49:50 +0000

    Word count test

--- scripts/LeNet-5.py
+++ scripts/LeNet-5.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D,
 from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
 
 
 # dimensions of our images.
-img_width, img_height = 150, 150
+img_width, img_height = 32, 32
 
 train_data_dir = '/content/chest_xray/train/'
 validation_data_dir = '/content/chest_xray/val/'
@@ -20,24 +20,19 @@ batch_size = 16
 input_shape = (img_width, img_height, 3)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
 
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(6, pool_size=14, strides=2))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (10, 10), activation='relu'))
+
+model.add(MaxPooling2D(16, pool_size=5, strides=2))
 
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+
+model.add(Dense(84, activation='relu'))
+
+model.add(Dense(2, activation="sigmoid"))
 
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
