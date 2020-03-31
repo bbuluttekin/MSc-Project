430d9edcddd74281c4b92e52d2537cd9bde1ea06
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2020-03-31 21:56:14 +0100

    Quiet mode for unzip

--- scripts/getdatadrive.sh
+++ scripts/getdatadrive.sh
@@ -2,8 +2,8 @@ pip install -q kaggle
 mkdir -p ~/.kaggle
 cp drive/My\ Drive/kaggle.json ~/.kaggle/
 kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
-unzip chest-xray-pneumonia.zip
-unzip chest_xray.zip
+unzip -qq chest-xray-pneumonia.zip
+unzip -qq chest_xray.zip
 rm chest-xray-pneumonia.zip
 rm chest_xray.zip
 rm -rf __MACOSX/
