56d7b65f57da958a80f50fc8b5e44a3ca138dbae
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2020-04-20 17:20:24 +0100

    Added git related processes to script

--- scripts/getdatadrive.sh
+++ scripts/getdatadrive.sh
@@ -3,8 +3,9 @@ mkdir -p ~/.kaggle
 cp drive/My\ Drive/kaggle.json ~/.kaggle/
 kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
 unzip -qq chest-xray-pneumonia.zip
-#unzip -qq chest_xray.zip
 rm chest-xray-pneumonia.zip
-#rm chest_xray.zip
 rm -rf chest_xray/__MACOSX/
 rm -rf sample_data/
+# below two line can be activated after repo made public
+# git clone https://github.com/bbuluttekin/MSc-Project.git
+# mv MSc-Project/src/ .
