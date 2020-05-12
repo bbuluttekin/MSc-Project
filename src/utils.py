fbd099c0440b657a02c3bd46d9350d5cf0bcdcfb
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2020-05-12 22:15:02 +0100

    Utility functions download/upload for s3

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import datetime
 
+import boto3
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -49,3 +50,39 @@ def plot_training_curves(training, validation, title, subplot):
     ax.set_ylabel(title)
     ax.set_xlabel('epoch')
     ax.legend(['train', 'valid.'])
+
+
+def download_from_s3(bucket_name, filename, targetname=None):
+    s3_client = boto3.client('s3')
+    if targetname:
+        with open(targetname, "wb") as f:
+            s3_client.download_fileobj(
+                bucket_name,
+                filename,
+                f
+            )
+    else:
+        with open(filename, "wb") as f:
+            s3_client.download_fileobj(
+                bucket_name,
+                filename,
+                f
+            )
+
+
+def upload_to_s3(bucket_name, filename, targetname=None):
+    s3_client = boto3.client('s3')
+    if targetname:
+        with open(filename, "rb") as f:
+            s3_client.upload_fileobj(
+                f,
+                bucket_name,
+                targetname
+            )
+    else:
+        with open(filename, "rb") as f:
+            s3_client.upload_fileobj(
+                f,
+                bucket_name,
+                filename
+            )
