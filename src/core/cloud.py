df18f0f2c2184e08fbf6a014ffb267a1c13c77ae
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2020-05-14 22:57:09 +0100

    Added accelerator detection and strategy initilization

--- src/core/cloud.py
+++ src/core/cloud.py
@@ -13,9 +13,38 @@ class CloudTrainer:
                 log_dir=self.log_path
             )
         ]
+        # TPU detection
+        try:
+            self.tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+            print('Running on TPU ', tpu.master())
+        except ValueError:
+            self.tpu = None
+            self.gpu = tf.config.experimental.list_logical_devices("GPU")
+        # Set the training strategy
+        if self.tpu:
+            tf.config.experimental_connect_to_cluster(self.tpu)
+            tf.tpu.experimental.initialize_tpu_system(self.tpu)
+            self.strategy = tf.distribute.experimental.TPUStrategy(self.tpu)
+        elif len(self.gpu) > 0:
+            self.strategy = tf.distribute.MirroredStrategy(self.gpu)
+            print('Running on ', len(self.gpu), ' GPU(s) ')
+        else:
+            self.strategy = tf.distribute.get_strategy()
+            print('Running on CPU')
 
-    def train_keras_seq_model(self, model):
+    def add_keras_seq_model(self, model):
         self.model = model
 
-    def train_keras_functional_model(self, model):
+    def add_keras_functional_model(self, model):
         self.model = model
+
+    def compile(self, optimizer, loss, metrics):
+        if self.model:
+            with self.strategy.scope():
+                self.model.compile(
+                    optimizer=optimizer,
+                    loss=loss,
+                    metrics=metrics
+                )  # Add args
+        else:
+            raise ValueError("Define model needs to be added")
