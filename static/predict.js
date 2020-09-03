8f91ca7ce614b05b7bae5b248cc514fcf6947308
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2020-09-03 23:04:21 +0100

    Full prediction page

--- static/predict.js
+++ static/predict.js
@@ -1,11 +1,10 @@
 var loadFile = function (event) {
+    $("#result").text("")
     var output = document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function () {
-        URL.revokeObjectURL(output.src) // free memory
+        URL.revokeObjectURL(output.src)
     }
-
-
     // $('#spinner').parent().hide()
 };
 
@@ -24,11 +23,23 @@ $("#output").change(function () {
 
 let model;
 (async function () {
-    model = await tf.loadLayersModel("");
+    model = await tf.loadLayersModel(
+        "https://bbuluttekin.github.io/MSc-Project/static/model.json");
     $('#spinner').parent().hide();
 })();
 
 var predict = async function () {
     let image = $("#output").get(0);
     let tensor = tf.browser.fromPixels(image);
+    let resizedTensor = tf.image.resizeBilinear(tensor, [224, 224])
+        .mul(1 / 255)
+        .expandDims();
+    let prediction = await model.predict(resizedTensor).data();
+    if (prediction <= 0.5) {
+        // Normal
+        $("#result").text("Result: No Pneumonia detected.")
+    } else {
+        // Pneumonia 
+        $("#result").text("Result: Pneumonia detected.")
+    }
 };
